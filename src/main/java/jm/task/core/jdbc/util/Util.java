bc978f763b29933839eedb652caf667105e9c2b9
DiKiYi <dkylak@gmail.com>
2023-03-11 16:10:13 +0300

    1.2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -13,26 +13,32 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/test1";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
-    public static Connection getConnections(){
+
+    public static Connection getConnections() {
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            return null;
         }
     }
 
-//    public static Configuration configuration () {
+//    public static Configuration getConfiguration () {
 //        Configuration configuration = new Configuration();
 //        Properties properties = new Properties();
 //        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/test1");
 //        properties.put(Environment.USER, "root");
 //        properties.put(Environment.PASS, "root");
 //        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+//        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+//        properties.put(Environment.SHOW_SQL, "true");
+//        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+//        properties.put(Environment.HBM2DDL_AUTO, "create-drop");
 //        configuration.setProperties(properties);
 //
 //
 //    }
 
 
-    // реализуйте настройку соеденения с БД
+//     реализуйте настройку соеденения с БД
 }
